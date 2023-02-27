3cba5868d5952fd8847a54a49c0debb8b158b317
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-02-27 10:28:58 +0400

    Initial commit

---
+++ src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Box(contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()){
+            val stopWatch = remember { StopWatch() }
+            StopWatchDisplay(
+                formattedTime = stopWatch.formattedTime,
+                onStartClick = stopWatch::start,
+                onPauseClick = stopWatch::pause,
+                onRestClick = stopWatch::reset
+            )
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
